d1f2438cfdbe4ab179dd719eb2c7ac9fd042636f
nuriguri <34469296+nuriguri@users.noreply.github.com>
2023-06-13 00:06:54 +0000

    Pending changes exported from your codespace

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
 import './App.css';
+import LottieAnimation from './LottieAnimation';
+import animationData from './space.json';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
+      <div>
+        <LottieAnimation animationData={animationData} />
+      </div>
+      {/* <header className="App-header">
+      
         <img src="Octocat.png" className="App-logo" alt="logo" />
         <p>
           GitHub Codespaces <span className="heart">♥️</span> React
@@ -21,7 +27,7 @@ function App() {
             Learn React
           </a>
         </p>
-      </header>
+      </header> */}
     </div>
   );
 }
